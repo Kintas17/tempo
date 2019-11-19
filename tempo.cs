de4291b1af3b9df96ce7954077c7f9437a0b87a8
Kintas17 <49915294+Kintas17@users.noreply.github.com>
2019-11-19 16:44:39 +0000

    Add files via upload

---
+++ tempo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tempovida
+{
+    class tempo
+    {
+        private DateTime tempodevida = new DateTime().Date;
+
+        public string recebe
+        {
+            set
+            {
+                tempodevida = Convert.ToDateTime(value);
+            }
+        }
+        public string envia
+        {
+            get
+            {
+                return "O dia da semana em que você nasceu foi " + tempodevida.ToString("g");
+            }
+        }
+    }
+}
